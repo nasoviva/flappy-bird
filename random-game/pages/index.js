06bfa483793749b8d2e15e47ff7bd75429cd849f
nasoviva <nasoviva@gmail.com>
2024-10-02 23:12:06 -0300

    feat: add sound and results of score

--- random-game/pages/index.js
+++ random-game/pages/index.js
@@ -16,44 +16,120 @@ enemyUp.src = "../assets/images/enemyUp.png";
 const enemyBottom = new Image();
 enemyBottom.src = "../assets/images/enemyBottom.png";
 
-const bird_audio = new Audio();
-bird_audio.src = "../assets/audio/bird.mp3";
+const music = new Audio();
+music.src = "../assets/audio/music.mp3";
 
-const score_audio = new Audio();
-score_audio.src = "../assets/audio/score.mp3";
+const win = new Audio();
+win.src = "../assets/audio/win.mp3";
 
-let x = 10;
+const lose = new Audio();
+lose.src = "../assets/audio/lose.mp3";
+
+const score_add = new Audio();
+score_add.src = "../assets/audio/score.mp3";
+
+let scores = [];
+localStorage.setItem("scores", JSON.stringify(scores));
+let savedScores = JSON.parse(localStorage.getItem("scores"));
+
+let x = 100;
 let y = 10;
 let score = 0;
 const gap = 100;
+const result = document.querySelector(".result");
+
+let enemy = [];
+enemy[0] = {
+  x: canvas.width,
+  y: 0,
+};
+
+function updateScoreTable() {
+  const scoreArr = document.querySelectorAll("td");
+
+  for (let i = 0; i < savedScores.length; i++) {
+    scoreArr[i + 1].textContent = savedScores[i];
+  }
+}
+
+function addScore(newScore) {
+  savedScores.push(newScore);
+  localStorage.setItem("scores", JSON.stringify(savedScores));
+  if (savedScores.length > 10) {
+    savedScores.shift();
+    localStorage.setItem("scores", JSON.stringify(savedScores));
+  }
+  updateScoreTable();
+}
+
+function start() {
+  alert("Start Game?");
+  document.addEventListener("keydown", () => {
+    if (!music.playing) {
+      music.play();
+    }
+  });
+}
+
+function restart() {
+  addScore(score);
+  music.pause();
+  if (score < 20) {
+    lose.play();
+    alert(`Game Over! Your score: ${score}`);
+  } else {
+    win.play();
+    alert(`You Win! Your score: ${score}`);
+  }
+  music.currentTime = 0;
+  score = 0;
+  y = 10;
+  enemy = [{ x: canvas.width, y: 0 }];
+  start();
+}
+
+updateScoreTable();
+start();
+
+function draw() {
+  context.drawImage(background, 0, 0, 700, 512);
+
+  for (let i = 0; i < enemy.length; i += 1) {
+    context.drawImage(enemyUp, enemy[i].x, enemy[i].y);
+    context.drawImage(
+      enemyBottom,
+      enemy[i].x,
+      enemy[i].y + enemyUp.height + gap,
+    );
+
+    enemy[i].x -= 1;
+
+    if (enemy[i].x === x - enemyUp.width) {
+      score++;
+      score_add.play();
+    }
+  }
+  context.drawImage(floor, 0, canvas.height - floor.height, 700, 512);
+  context.drawImage(bird, x, y);
+
+  y += 1;
+
+  if (
+    y === canvas.height - floor.height - bird.height ||
+    y === 0 ||
+    score === 20
+  ) {
+    restart();
+  }
+  result.textContent = "Score: " + score;
+  requestAnimationFrame(draw);
+}
+
+enemyBottom.onload = draw;
+music.play();
 
 document.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     y -= 15;
   }
 });
-
-let enemy = [];
-
-enemy[0] = {
-  x: canvas.width,
-  y: 0,
-};
-
-function draw() {
-  context.drawImage(background, 0, 0, 700, 512);
-  context.drawImage(enemyUp, 200, 0);
-  context.drawImage(enemyBottom, 200, enemyUp.height + gap);
-  context.drawImage(floor, 0, canvas.height - floor.height, 700, 512);
-  context.drawImage(bird, x, y);
-
-  y += 1;
-
-  if (y === canvas.height - floor.height - bird.height) {
-    location.reload();
-  }
-
-  requestAnimationFrame(draw);
-}
-
-enemyBottom.onload = draw;
